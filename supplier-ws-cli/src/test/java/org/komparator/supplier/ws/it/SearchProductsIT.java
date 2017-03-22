2339ba3555d3308936625e3abdb194a740577199
João Oliveira <joao.alexandre.oliveira@tecnico.ulisboa.pt>
2017-03-22 20:23:36 +0000

    Add tests to searchProducts method

--- supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
+++ supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
@@ -12,7 +12,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.komparator.supplier.ws.*;
-
 /**
  * Test suite
  */
@@ -22,11 +21,42 @@ public class SearchProductsIT extends BaseIT {
 
 	// one-time initialization and clean-up
 	@BeforeClass
-	public static void oneTimeSetUp() {
+	public static void oneTimeSetUp() throws BadProductId_Exception, BadProduct_Exception {
+		// clear remote service state before all tests
+		client.clear();
+
+		// fill-in test products
+		// (since getProduct is read-only the initialization below
+		// can be done once for all tests in this suite)
+		{
+			ProductView product = new ProductView();
+			product.setId("X1");
+			product.setDesc("Basketball");
+			product.setPrice(10);
+			product.setQuantity(10);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Y2");
+			product.setDesc("Soccer ball");
+			product.setPrice(20);
+			product.setQuantity(20);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Z3");
+			product.setDesc("Racket");
+			product.setPrice(30);
+			product.setQuantity(30);
+			client.createProduct(product);
+		}
 	}
 
 	@AfterClass
 	public static void oneTimeTearDown() {
+		client.clear();
 	}
 
 	// members
@@ -48,11 +78,60 @@ public class SearchProductsIT extends BaseIT {
 
 	// bad input tests
 
-	// TODO
-
+	@Test (expected = BadText_Exception.class)
+	public void nullDescription() throws BadText_Exception {
+		client.searchProducts(null);
+	}
+	
+	@Test (expected = BadText_Exception.class)
+	public void emptyDescription() throws BadText_Exception {
+		client.searchProducts("");
+	}
+	
+	@Test (expected = BadText_Exception.class)
+	public void whitespaceDescription() throws BadText_Exception {
+		client.searchProducts(" ");
+	}
+	
+	@Test (expected = BadText_Exception.class)
+	public void tabDescription() throws BadText_Exception {
+		client.searchProducts("\t");
+	}
+	
+	@Test (expected = BadText_Exception.class)
+	public void newLineDescription() throws BadText_Exception {
+		client.searchProducts("\n");
+	}
+	
+	
 	
 	// main tests
 
-	// TODO
+	@Test
+	public void searchProductExists() throws BadText_Exception {
+		String desc = "Basketball";
+		List<ProductView> searchResults = client.searchProducts(desc);
+		assertEquals(1, searchResults.size());
+		assertEquals("X1", searchResults.get(0).getId());
+		assertEquals(10, searchResults.get(0).getPrice());
+		assertEquals(10, searchResults.get(0).getQuantity());
+		assertEquals(desc, searchResults.get(0).getDesc());
+	}
+	
+	@Test
+	public void searchProductNotExists() throws BadText_Exception {
+		assertEquals(0, client.searchProducts("cenas fixes").size());
+	}
+	
+	@Test
+	public void searchProductTwoExist() throws BadText_Exception {
+		assertEquals(2, client.searchProducts("ball").size());
+	}
+	
+	@Test
+	public void searchProductLowerCase() throws BadText_Exception {
+		assertEquals(1, client.searchProducts("basketball").size());
+	}
+	
 
 }
