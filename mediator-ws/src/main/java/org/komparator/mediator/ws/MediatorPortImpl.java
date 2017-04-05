a5e0cf4bf0d966ea1d9fd0139d362ab00a5eeff2
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-04-05 18:12:49 +0100

    Refromatting + few fixes
    - MediatorPortImpl
      - Remove unneeded attributes (such as wsName and uddiUrl)
      - Change the constructor arguments accordingly
    - MediatorEndpointManager
      - Add getter for the uddiURL
      - Instantiate portImpl in constructor (no nulls anymore)
    - MediatorClient
      - Uncomment code after generating sources

--- mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
+++ mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
@@ -1,145 +1,149 @@
 package org.komparator.mediator.ws;
 
+import javax.jws.WebService;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDIRecord;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
-
-import javax.jws.WebService;
-
 import org.komparator.supplier.ws.BadText_Exception;
 import org.komparator.supplier.ws.ProductView;
 import org.komparator.supplier.ws.cli.SupplierClient;
 import org.komparator.supplier.ws.cli.SupplierClientException;
 
-import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
-import pt.ulisboa.tecnico.sdis.ws.uddi.UDDIRecord;
-
 
 @WebService(
-		endpointInterface = "org.komparator.mediator.ws.MediatorPortType", 
-		wsdlLocation = "mediator.1_0.wsdl", 
-		name = "MediatorWebService", 
-		portName = "MediatorPort", 
-		targetNamespace = "http://ws.mediator.komparator.org/", 
-		serviceName = "MediatorService"
+        endpointInterface = "org.komparator.mediator.ws.MediatorPortType",
+        wsdlLocation = "mediator.1_0.wsdl",
+        name = "MediatorWebService",
+        portName = "MediatorPort",
+        targetNamespace = "http://ws.mediator.komparator.org/",
+        serviceName = "MediatorService"
 )
 public class MediatorPortImpl implements MediatorPortType {
 
-	// end point manager
-	private MediatorEndpointManager endpointManager;
-	
-	private String uddiURL;
-	private String wsName;
-	
-	private List<SupplierClient> suppliers;
+    // end point manager
+    private MediatorEndpointManager endpointManager;
 
-	public MediatorPortImpl(MediatorEndpointManager endpointManager, String uddiURL, String wsName) {
-		this.endpointManager = endpointManager;
-		
-		this.uddiURL = uddiURL;
-		this.wsName = wsName;
-	}
+    private List<SupplierClient> suppliers;
 
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
+    public MediatorPortImpl(MediatorEndpointManager endpointManager) {
+        this.endpointManager = endpointManager;
+    }
 
-	@Override
-	public List<ItemView> getItems(String productId) throws InvalidItemId_Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public List<CartView> listCarts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	@Override
-	public List<ItemView> searchItems(String descText) throws InvalidText_Exception {
-		// check description text
-		if (descText == null)
-			throwInvalidText("Product description cannot be null!");
-		descText = descText.trim();
-		if (descText.length() == 0)
-			throwInvalidText("Product description cannot be empty or whitespace!");
-		
-		try {
-			supplierLookup();
-		} catch(MediatorException me) {
-			
-		} catch(SupplierClientException sce) {
-			
-		}
-		TreeSet<ItemView> products = new TreeSet<>(new ItemViewComparator());
-		
-		for(SupplierClient supplier : suppliers) {
-			List<ProductView> productviews = null;
-			try {
-				productviews = supplier.searchProducts(descText);
-			} catch (BadText_Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			if(productviews != null) {
-				for(ProductView pv : productviews) {
-					products.add(newItemView(pv, "supplier.getWsURL()"));
-				}
-			}
-			
-		}
-		
-		return new ArrayList<ItemView>(products);
-		
-	}
+    @Override
+    public List<ItemView> getItems(String productId) throws InvalidItemId_Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public ShoppingResultView buyCart(String cartId, String creditCardNr)
-			throws EmptyCart_Exception, InvalidCartId_Exception, InvalidCreditCard_Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<CartView> listCarts() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void addToCart(String cartId, ItemIdView itemId, int itemQty) throws InvalidCartId_Exception,
-			InvalidItemId_Exception, InvalidQuantity_Exception, NotEnoughItems_Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public List<ItemView> searchItems(String descText) throws InvalidText_Exception {
+        // check description text
+        if (descText == null)
+            throwInvalidText("Product description cannot be null!");
+        descText = descText.trim();
+        if (descText.length() == 0)
+            throwInvalidText("Product description cannot be empty or whitespace!");
 
-	@Override
-	public String ping(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        try {
+            supplierLookup();
+        } catch (MediatorException me) {
 
-	@Override
-	public List<ShoppingResultView> shopHistory() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	// View helpers ----------------------------------------------------------
-	
-	private ItemView newItemView(ProductView pv, String supplierName) {
-		ItemView view = new ItemView();
-		ItemIdView idview = new ItemIdView();
-		
-		idview.setProductId(pv.getId());
-		idview.setSupplierId(supplierName);
-		
-		view.setItemId(idview);
-		view.setDesc(pv.getDesc());
-		view.setPrice(pv.getPrice());
-		return view;
-	}
-	/*
+        } catch (SupplierClientException sce) {
+
+        }
+        TreeSet<ItemView> products = new TreeSet<>(new ItemViewComparator());
+
+        for (SupplierClient supplier : suppliers) {
+            List<ProductView> productviews = null;
+            try {
+                productviews = supplier.searchProducts(descText);
+            } catch (BadText_Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            if (productviews != null) {
+                for (ProductView pv : productviews) {
+                    products.add(newItemView(pv, "supplier.getWsURL()"));
+                }
+            }
+
+        }
+
+        return new ArrayList<ItemView>(products);
+
+    }
+
+    @Override
+    public ShoppingResultView buyCart(String cartId, String creditCardNr)
+            throws EmptyCart_Exception, InvalidCartId_Exception, InvalidCreditCard_Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addToCart(String cartId, ItemIdView itemId, int itemQty) throws InvalidCartId_Exception,
+            InvalidItemId_Exception, InvalidQuantity_Exception, NotEnoughItems_Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String ping(String arg0) {
+        try {
+            supplierLookup();
+        } catch (MediatorException | SupplierClientException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (SupplierClient supplier : suppliers) {
+            sb.append(String.format("Response from %s: ", supplier.getWsURL()));
+            sb.append(supplier.ping(arg0));
+            sb.append("%n");
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public List<ShoppingResultView> shopHistory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    // View helpers ----------------------------------------------------------
+
+    private ItemView newItemView(ProductView pv, String supplierName) {
+        ItemView view = new ItemView();
+        ItemIdView idview = new ItemIdView();
+
+        idview.setProductId(pv.getId());
+        idview.setSupplierId(supplierName);
+
+        view.setItemId(idview);
+        view.setDesc(pv.getDesc());
+        view.setPrice(pv.getPrice());
+        return view;
+    }
+    /*
 	private PurchaseView newPurchaseView(Purchase purchase) {
 		PurchaseView view = new PurchaseView();
 		view.setId(purchase.getPurchaseId());
@@ -149,22 +153,28 @@ public class MediatorPortImpl implements MediatorPortType {
 		return view;
 	}
 	*/
-	// Exception helpers -----------------------------------------------------
+    // Exception helpers -----------------------------------------------------
 
-	/** Helper method to throw new InvalidText exception */
-	private void throwInvalidText(final String message) throws InvalidText_Exception {
-		InvalidText faultInfo = new InvalidText();
-		faultInfo.message = message;
-		throw new InvalidText_Exception(message, faultInfo);
-	}
+    /**
+     * Helper method to throw new InvalidText exception
+     */
+    private void throwInvalidText(final String message) throws InvalidText_Exception {
+        InvalidText faultInfo = new InvalidText();
+        faultInfo.message = message;
+        throw new InvalidText_Exception(message, faultInfo);
+    }
 
-    /** UDDI lookup */
+    /**
+     * UDDI lookup
+     */
     private void supplierLookup() throws MediatorException, SupplierClientException {
-    	Collection<UDDIRecord> uddi_records = null;
+        Collection<UDDIRecord> uddiRecords = null;
+        String uddiURL = endpointManager.getUddiURL();
+
         try {
 
             UDDINaming uddiNaming = new UDDINaming(uddiURL);
-            uddi_records = uddiNaming.listRecords(wsName);
+            uddiRecords = uddiNaming.listRecords("%Supplier%");
 
         } catch (Exception e) {
             String msg = String.format("Failed supplier lookup on uddi at %s!",
@@ -172,41 +182,38 @@ public class MediatorPortImpl implements MediatorPortType {
             throw new MediatorException(msg, e);
         }
 
-        if (uddi_records == null) {
+        if (uddiRecords == null) {
             String msg = String.format(
-                    "Suppliers with name %s not found on UDDI at %s", wsName,
-                    uddiURL);
+                    "Suppliers with name estupido not found on UDDI at %s", uddiURL);
             throw new MediatorException(msg);
-        }
-        else {
-        	suppliers = new ArrayList<>();
-        	for(UDDIRecord element : uddi_records) {
-        		try {
-        			this.suppliers.add(new SupplierClient(this.uddiURL, element.getOrgName()));
-        		}
-        		catch (SupplierClientException sce) {
-        			//Possibly do stuff
-        		}
-        	}
+        } else {
+            suppliers = new ArrayList<>();
+            for (UDDIRecord element : uddiRecords) {
+                try {
+                    this.suppliers.add(new SupplierClient(uddiURL, element.getOrgName()));
+                } catch (SupplierClientException sce) {
+                    //Possibly do stuff
+                }
+            }
         }
     }
-    
-    class ItemViewComparator implements Comparator<ItemView>{
-    	 
+
+    class ItemViewComparator implements Comparator<ItemView> {
+
         @Override
         public int compare(ItemView iv1, ItemView iv2) {
-        	String pId1 = iv1.getItemId().getProductId();
-        	String pId2 = iv2.getItemId().getProductId();
-        	
-        	int iP1 = iv1.getPrice();
-        	int iP2 = iv2.getPrice();
-        	
-        	int ola = pId1.compareTo(pId2);
-        	
-        	if(ola != 0){
-        		return ola;
-        	} 
-        	return (iP1>=iP2 ? 1 : -1);
-    	}
+            String pId1 = iv1.getItemId().getProductId();
+            String pId2 = iv2.getItemId().getProductId();
+
+            int iP1 = iv1.getPrice();
+            int iP2 = iv2.getPrice();
+
+            int ola = pId1.compareTo(pId2);
+
+            if (ola != 0) {
+                return ola;
+            }
+            return (iP1 >= iP2 ? 1 : -1);
+        }
     }
 }
