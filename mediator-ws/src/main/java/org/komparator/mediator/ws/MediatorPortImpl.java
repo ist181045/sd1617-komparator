f265c3f534b7c697bd80f58a4e77414eca5afb8c
Pedro Cerejo <pedro.cerejo@tecnico.ulisboa.pt>
2017-04-05 17:21:33 +0100

    Implement ﻿searchItems on mediator
    Start #9

--- mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
+++ mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
@@ -2,15 +2,20 @@ package org.komparator.mediator.ws;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
 
 import javax.jws.WebService;
 
+import org.komparator.supplier.ws.BadText_Exception;
+import org.komparator.supplier.ws.ProductView;
 import org.komparator.supplier.ws.cli.SupplierClient;
 import org.komparator.supplier.ws.cli.SupplierClientException;
 
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDIRecord;
+
 
 @WebService(
 		endpointInterface = "org.komparator.mediator.ws.MediatorPortType", 
@@ -57,8 +62,41 @@ public class MediatorPortImpl implements MediatorPortType {
 
 	@Override
 	public List<ItemView> searchItems(String descText) throws InvalidText_Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// check description text
+		if (descText == null)
+			throwInvalidText("Product description cannot be null!");
+		descText = descText.trim();
+		if (descText.length() == 0)
+			throwInvalidText("Product description cannot be empty or whitespace!");
+		
+		try {
+			supplierLookup();
+		} catch(MediatorException me) {
+			
+		} catch(SupplierClientException sce) {
+			
+		}
+		TreeSet<ItemView> products = new TreeSet<>(new ItemViewComparator());
+		
+		for(SupplierClient supplier : suppliers) {
+			List<ProductView> productviews = null;
+			try {
+				productviews = supplier.searchProducts(descText);
+			} catch (BadText_Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(productviews != null) {
+				for(ProductView pv : productviews) {
+					products.add(newItemView(pv, "supplier.getWsURL()"));
+				}
+			}
+			
+		}
+		
+		return new ArrayList<ItemView>(products);
+		
 	}
 
 	@Override
@@ -86,6 +124,39 @@ public class MediatorPortImpl implements MediatorPortType {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	// View helpers ----------------------------------------------------------
+	
+	private ItemView newItemView(ProductView pv, String supplierName) {
+		ItemView view = new ItemView();
+		ItemIdView idview = new ItemIdView();
+		
+		idview.setProductId(pv.getId());
+		idview.setSupplierId(supplierName);
+		
+		view.setItemId(idview);
+		view.setDesc(pv.getDesc());
+		view.setPrice(pv.getPrice());
+		return view;
+	}
+	/*
+	private PurchaseView newPurchaseView(Purchase purchase) {
+		PurchaseView view = new PurchaseView();
+		view.setId(purchase.getPurchaseId());
+		view.setProductId(purchase.getProductId());
+		view.setQuantity(purchase.getQuantity());
+		view.setUnitPrice(purchase.getUnitPrice());
+		return view;
+	}
+	*/
+	// Exception helpers -----------------------------------------------------
+
+	/** Helper method to throw new InvalidText exception */
+	private void throwInvalidText(final String message) throws InvalidText_Exception {
+		InvalidText faultInfo = new InvalidText();
+		faultInfo.message = message;
+		throw new InvalidText_Exception(message, faultInfo);
+	}
 
     /** UDDI lookup */
     private void supplierLookup() throws MediatorException, SupplierClientException {
@@ -108,6 +179,7 @@ public class MediatorPortImpl implements MediatorPortType {
             throw new MediatorException(msg);
         }
         else {
+        	suppliers = new ArrayList<>();
         	for(UDDIRecord element : uddi_records) {
         		try {
         			this.suppliers.add(new SupplierClient(this.uddiURL, element.getOrgName()));
@@ -118,5 +190,23 @@ public class MediatorPortImpl implements MediatorPortType {
         	}
         }
     }
-
+    
+    class ItemViewComparator implements Comparator<ItemView>{
+    	 
+        @Override
+        public int compare(ItemView iv1, ItemView iv2) {
+        	String pId1 = iv1.getItemId().getProductId();
+        	String pId2 = iv2.getItemId().getProductId();
+        	
+        	int iP1 = iv1.getPrice();
+        	int iP2 = iv2.getPrice();
+        	
+        	int ola = pId1.compareTo(pId2);
+        	
+        	if(ola != 0){
+        		return ola;
+        	} 
+        	return (iP1>=iP2 ? 1 : -1);
+    	}
+    }
 }
