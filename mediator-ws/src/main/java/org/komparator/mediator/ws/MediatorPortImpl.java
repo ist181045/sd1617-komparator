9af84c4bcc843635340848f1579c1fd81a4c2084
Pedro Cerejo <pedro.cerejo@tecnico.ulisboa.pt>
2017-04-05 14:34:01 +0100

    Add MediatorPortType interface methods

--- mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
+++ mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
@@ -1,10 +1,18 @@
 package org.komparator.mediator.ws;
 
+import java.util.List;
+
 import javax.jws.WebService;
 
-// TODO annotate to bind with WSDL
-// TODO implement port type interface
-public class MediatorPortImpl {
+@WebService(
+		endpointInterface = "org.komparator.mediator.ws.MediatorPortType", 
+		wsdlLocation = "mediator.1_0.wsdl", 
+		name = "MediatorWebService", 
+		portName = "MediatorPort", 
+		targetNamespace = "http://ws.mediator.komparator.org/", 
+		serviceName = "MediatorService"
+)
+public class MediatorPortImpl implements MediatorPortType {
 
 	// end point manager
 	private MediatorEndpointManager endpointManager;
@@ -13,23 +21,56 @@ public class MediatorPortImpl {
 		this.endpointManager = endpointManager;
 	}
 
-	// Main operations -------------------------------------------------------
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 
-    // TODO
-	
-    
-	// Auxiliary operations --------------------------------------------------	
-	
-    // TODO
+	@Override
+	public List<ItemView> getItems(String productId) throws InvalidItemId_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CartView> listCarts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ItemView> searchItems(String descText) throws InvalidText_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ShoppingResultView buyCart(String cartId, String creditCardNr)
+			throws EmptyCart_Exception, InvalidCartId_Exception, InvalidCreditCard_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addToCart(String cartId, ItemIdView itemId, int itemQty) throws InvalidCartId_Exception,
+			InvalidItemId_Exception, InvalidQuantity_Exception, NotEnoughItems_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String ping(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoppingResultView> shopHistory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
-	// View helpers -----------------------------------------------------
-	
-    // TODO
-
-    
-	// Exception helpers -----------------------------------------------------
-
-    // TODO
 
 }
