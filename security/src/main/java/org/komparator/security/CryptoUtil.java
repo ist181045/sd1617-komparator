f06a191dde7403add10a88f1448f41f8b376a69b
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-05-04 00:38:16 +0100

    Implement asymCipher + asymDecipher
    Seemed straight forward, closes both #28 and #29

--- security/src/main/java/org/komparator/security/CryptoUtil.java
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -1,12 +1,60 @@
 package org.komparator.security;
 
-import java.io.*;
-import java.security.*;
-import javax.crypto.*;
-import java.util.*;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class CryptoUtil {
+    private static final String RSA_PKCS1_TRANSFORMATION =
+            "RSA/ECB/PKCS1Padding";
 
-    // TODO add security helper methods
+    public static byte[] asymCipher(PrivateKey privateKey, byte[] plainBytes) {
+        return doRSACipher(Cipher.ENCRYPT_MODE, privateKey, plainBytes);
+    }
 
+    public static byte[] asymDecipher(PublicKey publicKey,
+            byte[] encryptedBytes) {
+        return doRSACipher(Cipher.DECRYPT_MODE, publicKey,
+                encryptedBytes);
+    }
+
+    private static byte[] doRSACipher(int opMode, Key key, byte[] bytes) {
+        return doCipherOperation(opMode, RSA_PKCS1_TRANSFORMATION, key, bytes);
+    }
+
+    private static byte[] doCipherOperation(int opMode, String transformation,
+            Key key, byte[] bytes) {
+        try {
+            Cipher cipher = Cipher.getInstance(transformation);
+            cipher.init(opMode, key);
+
+            return cipher.doFinal(bytes);
+        } catch (InvalidKeyException | IllegalBlockSizeException |
+                NoSuchAlgorithmException | BadPaddingException |
+                NoSuchPaddingException e) {
+            StringBuilder esb = new StringBuilder("Error ");
+
+            switch (opMode) {
+                case Cipher.DECRYPT_MODE:
+                    esb.append("decrypting ");
+                    break;
+                case Cipher.ENCRYPT_MODE:
+                    esb.append("encrypting ");
+                    break;
+            }
+            esb.append(
+                    String.format("bytes, caught %s: %s%n", e, e.getMessage()));
+
+            System.err.println(esb);
+        }
+
+        return null;
+    }
 }
