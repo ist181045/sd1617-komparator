9eac9be22e9176bba486a6a33df004e96ab7ebbc
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-05-10 20:21:02 +0100

    Refactor BuyCartArgsIT and AddToCartArgsIT
    Decision on which exception to be thrown is made before running the test

--- mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/BuyCartArgsIT.java
+++ mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/BuyCartArgsIT.java
@@ -11,7 +11,9 @@ import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 import org.komparator.mediator.ws.EmptyCart_Exception;
+import org.komparator.mediator.ws.InvalidCartId;
 import org.komparator.mediator.ws.InvalidCartId_Exception;
+import org.komparator.mediator.ws.InvalidCreditCard;
 import org.komparator.mediator.ws.InvalidCreditCard_Exception;
 
 @RunWith(Parameterized.class)
@@ -29,41 +31,42 @@ public class BuyCartArgsIT extends BaseIT {
     @Parameter(1)
     public String creditCardNr;
 
-    @Parameter(2)
-    public Class<Throwable> exception;
-
-    @Parameter(3) /* Unused */
+    @Parameter(2) /* Unused */
     public String testName;
 
-    @Parameters(name = "{3}")
+    @Parameters(name = "{2}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][] {
-            { null,           CC_NUMBER, InvalidCartId_Exception.class, "Cart ID: Null"             },
-            { "",             CC_NUMBER, InvalidCartId_Exception.class, "Cart ID: Empty String"     },
-            { "  ",           CC_NUMBER, InvalidCartId_Exception.class, "Cart ID: Spaces"           },
-            { "\n",           CC_NUMBER, InvalidCartId_Exception.class, "Cart ID: Linefeed"         },
-            { "\t",           CC_NUMBER, InvalidCartId_Exception.class, "Cart ID: Horizontal Tab"   },
-            { "\r",           CC_NUMBER, InvalidCartId_Exception.class, "Cart ID: Carriage Return"  },
-            { "abc\r\n\tabc", CC_NUMBER, InvalidCartId_Exception.class, "Cart ID: Mixed Whitespace" },
+            { null,           CC_NUMBER, "Cart ID: Null"             },
+            { "",             CC_NUMBER, "Cart ID: Empty String"     },
+            { "  ",           CC_NUMBER, "Cart ID: Spaces"           },
+            { "\n",           CC_NUMBER, "Cart ID: Linefeed"         },
+            { "\t",           CC_NUMBER, "Cart ID: Horizontal Tab"   },
+            { "\r",           CC_NUMBER, "Cart ID: Carriage Return"  },
+            { "abc\r\n\tabc", CC_NUMBER, "Cart ID: Mixed Whitespace" },
 
-            { CART_ID,           null, InvalidCreditCard_Exception.class, "CC Number: Null"              },
-            { CART_ID,             "", InvalidCreditCard_Exception.class, "CC Number: Empty String"      },
-            { CART_ID,           "  ", InvalidCreditCard_Exception.class, "CC Number: Spaces"            },
-            { CART_ID,           "\n", InvalidCreditCard_Exception.class, "CC Number: Linefeed"          },
-            { CART_ID,           "\t", InvalidCreditCard_Exception.class, "CC Number: Horizontal Tab"    },
-            { CART_ID,           "\r", InvalidCreditCard_Exception.class, "CC Number: Carriage Return"   },
-            { CART_ID, "abc\r\n\tabc", InvalidCreditCard_Exception.class, "CC Number: Mixed Whitespace"  },
-            { CART_ID, "123456789012", InvalidCreditCard_Exception.class, "CC Number: Invalid Number"    },
-            { CART_ID,      "4012888", InvalidCreditCard_Exception.class, "CC Number: Incomplete Number" },
+            { CART_ID,           null, "CC Number: Null"              },
+            { CART_ID,             "", "CC Number: Empty String"      },
+            { CART_ID,           "  ", "CC Number: Spaces"            },
+            { CART_ID,           "\n", "CC Number: Linefeed"          },
+            { CART_ID,           "\t", "CC Number: Horizontal Tab"    },
+            { CART_ID,           "\r", "CC Number: Carriage Return"   },
+            { CART_ID, "abc\r\n\tabc", "CC Number: Mixed Whitespace"  },
+            { CART_ID, "123456789012", "CC Number: Invalid Number"    },
+            { CART_ID,      "4012888", "CC Number: Incomplete Number" },
 
-            { CART_ID,      CC_NUMBER, InvalidCartId_Exception.class, "Cart: Doesnt Exist" }
+            { CART_ID,      CC_NUMBER, "Cart ID: Doesn't Exist" }
         });
     }
 
     @Test
-    public void testArgs() throws InvalidCreditCard_Exception,
-            EmptyCart_Exception, InvalidCartId_Exception {
-        thrown.expect(exception);
+    public void testArgs() throws Throwable {
+        if (creditCardNr == CC_NUMBER) { // It's OK! Don't panic
+            thrown.expect(InvalidCartId_Exception.class);
+        } else {
+            thrown.expect(InvalidCreditCard_Exception.class);
+        }
+
         mediatorClient.buyCart(cartId, creditCardNr);
     }
 }
