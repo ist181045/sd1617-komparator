2480be6662b35e61b49516d8b41b046d2ce6a2fa
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-05-18 20:29:58 +0100

    Fix (for sure) MessageIdHandler, set property scope to APPLICATION

--- security/src/main/java/org/komparator/security/handler/MessageIdHandler.java
+++ security/src/main/java/org/komparator/security/handler/MessageIdHandler.java
@@ -11,6 +11,7 @@ import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.soap.SOAPPart;
 import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.MessageContext.Scope;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 
@@ -53,6 +54,13 @@ public class MessageIdHandler implements SOAPHandler<SOAPMessageContext> {
         String prefix = service.getLocalPart().substring(0, 3).toLowerCase();
         String uri = service.getNamespaceURI();
 
+        QName operation = (QName)smc.get(MessageContext.WSDL_OPERATION);
+        String opName = operation.getLocalPart();
+
+        if (!(opName.equals(OPERATION_ADDTOCART)
+                || opName.equals(OPERATION_BUYCART)))
+            return true;
+
         SOAPMessage msg = smc.getMessage();
         SOAPPart sp = msg.getSOAPPart();
         SOAPEnvelope se;
@@ -67,43 +75,39 @@ public class MessageIdHandler implements SOAPHandler<SOAPMessageContext> {
             throw new RuntimeException(error);
         }
 
-        QName operation = (QName)smc.get(MessageContext.WSDL_OPERATION);
-        String opName = operation.getLocalPart();
-        if (opName.equals(OPERATION_ADDTOCART)
-                || opName.equals(OPERATION_BUYCART)) {
-            if (outbound) {
-                String sender = SecurityManager.getInstance().getSender();
-                if (sender.equals("MediatorClient")) {
-                    String messageId = (String)smc.get(MESSAGE_ID_PROPERTY);
-                    if (messageId == null) {
-                        String error =
-                                "Couldn't find message id in message context";
-                        System.err.println(error);
-                        throw new RuntimeException(error);
-                    }
-
-                    try {
-                        Name name = se.createName(localName, prefix, uri);
-                        SOAPHeaderElement element = sh.addHeaderElement(name);
-                        element.addTextNode(messageId);
-                        msg.saveChanges();
-                    } catch (SOAPException e) {
-                        String error = "SOAP error adding message id to header: "
-                                + e.getMessage();
-                        System.err.println(error);
-                        throw new RuntimeException(error);
-                    }
-                }
-            } else {
-                NodeList nodes = sh.getElementsByTagNameNS(uri, localName);
-                if (nodes.getLength() > 0) {
-                    String messageId = nodes.item(0).getTextContent();
-                    smc.put(MESSAGE_ID_PROPERTY, messageId);
-                } else {
-                    String error = "Couldn't add message id to message context";
+        String sender = SecurityManager.getInstance().getSender();
+        if (outbound) {
+            if (sender.equals("MediatorClient")) {
+                String messageId = (String)smc.get(MESSAGE_ID_PROPERTY);
+                if (messageId == null) {
+                    String error =
+                            "Couldn't find message id in message context";
                     System.err.println(error);
                     throw new RuntimeException(error);
                 }
+
+                try {
+                    Name name = se.createName(localName, prefix, uri);
+                    SOAPHeaderElement element = sh.addHeaderElement(name);
+                    element.addTextNode(messageId);
+                    msg.saveChanges();
+                } catch (SOAPException e) {
+                    String error = "SOAP error adding message id to header: "
+                            + e.getMessage();
+                    System.err.println(error);
+                    throw new RuntimeException(error);
+                }
+            }
+        } else if (!sender.equals("MediatorClient")) {
+            NodeList nodes = sh.getElementsByTagNameNS(uri, localName);
+            if (nodes.getLength() > 0) {
+                String messageId = nodes.item(0).getTextContent();
+                smc.put(MESSAGE_ID_PROPERTY, messageId);
+                smc.setScope(MESSAGE_ID_PROPERTY, Scope.APPLICATION);
+            } else {
+                String error = "Couldn't add message id to message context";
+                System.err.println(error);
+                throw new RuntimeException(error);
             }
         }
 
