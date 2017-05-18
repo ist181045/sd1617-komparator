d1be1c35229db62d81952c6e0aa2385553856909
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-05-18 18:31:16 +0100

    Refactor MessageIdHandler.. in a sense (more like defactor)

--- security/src/main/java/org/komparator/security/handler/MessageIdHandler.java
+++ security/src/main/java/org/komparator/security/handler/MessageIdHandler.java
@@ -1,15 +1,6 @@
 package org.komparator.security.handler;
 
-import static javax.xml.bind.DatatypeConverter.printBase64Binary;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.Set;
-
-import org.komparator.security.*;
-import org.komparator.security.SecurityManager;
-import org.w3c.dom.NodeList;
 
 import javax.xml.namespace.QName;
 import javax.xml.soap.Name;
@@ -23,11 +14,15 @@ import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 
+import org.komparator.security.SecurityManager;
+import org.w3c.dom.NodeList;
+
 /**
  * This SOAPHandler outputs the contents of inbound and outbound messages.
  */
 public class MessageIdHandler implements SOAPHandler<SOAPMessageContext> {
-	public static final String MESSAGE_ID_PROPERTY = "org.komparator.mediator.ws.message.id";
+	private static final String MESSAGE_ID_PROPERTY =
+            "org.komparator.mediator.ws.message.id";
 	
 	private static final String OPERATION_BUYCART = "buyCart";
 	private static final String OPERATION_ADDTOCART = "addToCart";
@@ -52,12 +47,66 @@ public class MessageIdHandler implements SOAPHandler<SOAPMessageContext> {
 	@Override
 	public boolean handleMessage(SOAPMessageContext smc) {
 		Boolean outbound = (Boolean) smc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
-		
-		if(outbound)
-			outMessageId(smc);
-		else
-			inMessageId(smc);
-		
+        QName service = (QName)smc.get(MessageContext.WSDL_SERVICE);
+
+        String localName = "MessageId";
+        String prefix = service.getLocalPart().substring(0, 3).toLowerCase();
+        String uri = service.getNamespaceURI();
+
+        SOAPMessage msg = smc.getMessage();
+        SOAPPart sp = msg.getSOAPPart();
+        SOAPEnvelope se;
+        SOAPHeader sh;
+        try {
+            se = sp.getEnvelope();
+            sh = se.getHeader();
+        } catch (SOAPException soape) {
+            String error = "SOAP error retrieving envelope/header: "
+                    + soape.getMessage();
+            System.err.println(error);
+            throw new RuntimeException(error);
+        }
+
+        QName operation = (QName)smc.get(MessageContext.WSDL_OPERATION);
+        String opName = operation.getLocalPart();
+        if (opName.equals(OPERATION_ADDTOCART)
+                || opName.equals(OPERATION_BUYCART)) {
+            if (outbound) {
+                String sender = SecurityManager.getInstance().getSender();
+                if (sender.equals("MediatorClient")) {
+                    String messageId = (String)smc.get(MESSAGE_ID_PROPERTY);
+                    if (messageId == null) {
+                        String error =
+                                "Couldn't find message id in message context";
+                        System.err.println(error);
+                        throw new RuntimeException(error);
+                    }
+
+                    try {
+                        Name name = se.createName(localName, prefix, uri);
+                        SOAPHeaderElement element = sh.addHeaderElement(name);
+                        element.addTextNode(messageId);
+                        msg.saveChanges();
+                    } catch (SOAPException e) {
+                        String error = "SOAP error adding message id to header: "
+                                + e.getMessage();
+                        System.err.println(error);
+                        throw new RuntimeException(error);
+                    }
+                }
+            } else {
+                NodeList nodes = sh.getElementsByTagNameNS(localName, uri);
+                if (nodes.getLength() > 0) {
+                    String messageId = nodes.item(0).getTextContent();
+                    smc.put(MESSAGE_ID_PROPERTY, messageId);
+                } else {
+                    String error = "Couldn't add message id to message context";
+                    System.err.println(error);
+                    throw new RuntimeException(error);
+                }
+            }
+        }
+
 		return true;
 	}
 
@@ -75,77 +124,4 @@ public class MessageIdHandler implements SOAPHandler<SOAPMessageContext> {
 	public void close(MessageContext messageContext) {
 		// nothing to clean up
 	}
-
-	/**
-	 * Check the MESSAGE_OUTBOUND_PROPERTY in the context to see if this is an
-	 * outgoing or incoming message. Write a brief message to the print stream
-	 * and output the message. The newTransformer() method can throw a
-	 * TransformerConfigurationExcpetion while the transform() method can throw
-	 * a TransformerException (both caught and ignored).
-	 */
-	private void outMessageId(SOAPMessageContext smc) {
-		QName service = (QName)smc.get(MessageContext.WSDL_SERVICE);
-		
-		String localName = "MessageId";
-		String prefix = service.getLocalPart().substring(0, 3).toLowerCase();
-		String uri = service.getNamespaceURI();
-		
-		System.out.println(service.getLocalPart());
-
-		SOAPMessage msg = smc.getMessage();
-		SOAPPart sp = msg.getSOAPPart();
-		try {
-			SOAPEnvelope se = sp.getEnvelope();
-			SOAPHeader sh = se.getHeader();
-
-			String sender = SecurityManager.getInstance().getSender();
-			
-			QName operation = (QName) smc.get(MessageContext.WSDL_OPERATION);
-
-			System.out.println(operation.getLocalPart());
-			
-			
-			if (sender.equals("MediatorClient") && (operation.equals(OPERATION_BUYCART) || operation.equals(OPERATION_ADDTOCART))) {
-				String messageId = (String) smc.get(MESSAGE_ID_PROPERTY);
-				if (messageId == null) return;
-
-				Name name = se.createName(localName, prefix, uri);
-				SOAPHeaderElement element = sh.addHeaderElement(name);
-				element.addTextNode(messageId);
-				msg.saveChanges();
-				
-			}
-		} catch (SOAPException e) {
-			System.err.println("bork");
-		}
-
-	}
-	
-	private void inMessageId(SOAPMessageContext smc) {
-		QName service = (QName)smc.get(MessageContext.WSDL_SERVICE);
-
-		String localName = "MessageId";
-		String prefix = service.getLocalPart().substring(0, 3).toLowerCase();
-		String uri = service.getNamespaceURI();
-		
-		
-		SOAPMessage msg = smc.getMessage();
-		
-		try {
-			SOAPHeader sh = msg.getSOAPPart().getEnvelope().getHeader();
-			NodeList nodes = sh.getElementsByTagNameNS(uri, localName);
-			
-			if(nodes.getLength() == 0) return;
-			
-			String messageId = nodes.item(0).getTextContent();
-			
-			smc.put(MESSAGE_ID_PROPERTY, messageId);
-			
-			
-		} catch (SOAPException e) {
-			System.err.println("bork");
-		}
-		
-	}
-
 }
